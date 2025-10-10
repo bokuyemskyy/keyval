53ab0e0ff0620b0b79da03596e2ca24366d2573d
Ivan Bokuyemskyy <ivan.bokuyemskyy@gmail.com>
2025-10-10 22:16:29 +0200

    chore: removed build from repo

--- build/third_party/catch2/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/amon/Code/keyval/third_party/catch2
-# Build directory: /home/amon/Code/keyval/build/third_party/catch2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
