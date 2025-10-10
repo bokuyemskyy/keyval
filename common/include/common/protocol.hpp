bf58ce9be63b3d994dbf86dccb7c12118c0be100
Ivan Bokuyemskyy <ivan.bokuyemskyy@gmail.com>
2025-10-10 22:14:14 +0200

    feat: redis-compliant protocol

--- common/include/common/protocol.hpp
+++ common/include/common/protocol.hpp
@@ -1,16 +1,48 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <sstream>
+#include <variant>
+#include <memory>
 
-struct Command
+struct Request
 {
-    std::string name;
+    std::string command;
     std::vector<std::string> args;
+};
+
+struct Response;
+using ResponseValue = std::variant<
+    std::monostate,
+    std::string,
+    long long,
+    std::vector<std::shared_ptr<Response>>>;
+
+enum class ResponseType
+{
+    SIMPLE_STRING,
+    ERROR,
+    INTEGER,
+    BULK_STRING,
+    ARRAY,
+    NIL,
+    SHUTDOWN
+};
+
+struct Response
+{
+    ResponseType type;
+    ResponseValue value;
 };
 
 class Protocol
 {
 public:
-    static Command deserialize(const std::string &data);
-    static std::string serialize(const Command &cmd);
+    static Request deserializeRequest(const std::string &input);
+    static std::string serializeResponse(const Response &response);
+
+private:
+    static std::vector<std::string> parseArray(const std::string &input);
+    static std::vector<std::string> parseInline(const std::string &input);
+    static void getlineCRLF(std::istringstream &stream, std::string &line);
 };
